4757ff986af981f7e7a77eca8ebbbc69cb1cdabf
Abi <kabishake191@gmail.com>
2025-03-05 20:08:48 +0530

    Initial commit

--- app.py
+++ app.py
@@ -90,5 +90,6 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     # Get port from environment (for deployment) or default to 5000
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(host="0.0.0.0", port=port, debug=True)
+
